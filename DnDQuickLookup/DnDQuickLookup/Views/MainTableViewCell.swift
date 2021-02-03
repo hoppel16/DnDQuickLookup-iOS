7cfdddc2296f1e4509b9ea5776ca50b06dbccff0
Hunter Oppel <hunter.oppel@gmail.com>
2021-02-02 19:08:33 -0500

    Basic table view setup

---
+++ DnDQuickLookup/DnDQuickLookup/Views/MainTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MainTableViewCell.swift
+//  DnDQuickLookup
+//
+//  Created by Hunter Oppel on 2/2/21.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+    @IBOutlet var mainString: UILabel!
+}
